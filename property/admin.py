a72809c2b50f392dfa334b89e46c6d9215336a46
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-12-29 19:36:27 +0300

    Convert tabs to spaces

--- property/admin.py
+++ property/admin.py
@@ -4,10 +4,10 @@ from .models import Flat
 
 
 class FlatAdmin(admin.ModelAdmin):
-	search_fields = ['town', 'address']
-	readonly_fields = ['created_at']
-	list_display = ['town', 'address', 'price', 'new_building', 'construction_year']
-	list_editable = ['new_building']
+    search_fields = ['town', 'address']
+    readonly_fields = ['created_at']
+    list_display = ['town', 'address', 'price', 'new_building', 'construction_year']
+    list_editable = ['new_building']
 
 
 admin.site.register(Flat, FlatAdmin)
