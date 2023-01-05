370e81ca8396da3c0adfd77ac1d1579f266e39f5
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-01-05 17:07:31 +0300

    Add blank=True to some secondary fields

--- property/models.py
+++ property/models.py
@@ -50,7 +50,8 @@ class Flat(models.Model):
     liked_by = models.ManyToManyField(
         User,
         verbose_name='Кто лайкнул',
-        related_name='liked_flats')
+        related_name='liked_flats',
+        blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
@@ -93,6 +94,7 @@ class Owner(models.Model):
         verbose_name='Квартиры в собственности',
         related_name='owned_by',
         db_index=True,
+        blank=True,
     )
 
     def __str__(self):
